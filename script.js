4f20fd9708271cf09b0b64750dcf091d34c8ed9c
Your Name <you@example.com>
2016-09-29 09:47:46 -0500

    complete base level

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function Fruit (name, price) {
 	}
 
 
-var apple = new Fruit ('apple', 9.50);
-var orange = new Fruit ('orange', 9.50);
-var banana = new Fruit ('banana', .50);
-var grape = new Fruit ('grape', .50);
+var apple = new Fruit ('apple', 5);
+var orange = new Fruit ('orange', 5);
+var banana = new Fruit ('banana', 5);
+var grape = new Fruit ('grape', 5);
 
-var totalValue;
+var totalValue = 100;
 
  var buyerApple = {
 	 buyerAppleAmount: 0,
@@ -41,11 +41,63 @@ myFunction();
 $("#apple").on('click', 'button', function () {
 	if (totalValue > apple.price) {
 		totalValue -= apple.price;
-		buyerAppleAmount++;
-		buyerAppleTotalPrice+=apple.price;
-		buyerAppleAveragePrice= buyerAppleTotalPrice/buyerAppleAmount;
+		console.log('total value', totalValue);
+		$('#totalValue').text(totalValue.toFixed(2));
+		buyerApple.buyerAppleAmount++;
+		$('#appleAmount').text(buyerApple.buyerAppleAmount);
+		buyerApple.buyerAppleTotalPrice+=apple.price;
+		$('#totalApplePrice').text((buyerApple.buyerAppleTotalPrice).toFixed(2));
+		buyerApple.buyerAppleAveragePrice=buyerApple.buyerAppleTotalPrice/buyerApple.buyerAppleAmount;
+		$('#averageApplePrice').text((buyerApple.buyerAppleAveragePrice).toFixed(2));
+	} else {
+		alert("You do not have enough money for that item!");
 	}
-})
+});
+$("#orange").on('click', 'button', function () {
+	if (totalValue > orange.price) {
+		totalValue -= orange.price;
+		console.log('total value', totalValue);
+		$('#totalValue').text(totalValue.toFixed(2));
+		buyerOrange.buyerOrangeAmount++;
+		$('#orangeAmount').text(buyerOrange.buyerOrangeAmount);
+		buyerOrange.buyerOrangeTotalPrice+=orange.price;
+		$('#totalOrangePrice').text((buyerOrange.buyerOrangeTotalPrice).toFixed(2));
+		buyerOrange.buyerOrangeAveragePrice=buyerOrange.buyerOrangeTotalPrice/buyerOrange.buyerOrangeAmount;
+		$('#averageOrangePrice').text((buyerOrange.buyerOrangeAveragePrice).toFixed(2));
+	} else {
+		alert("You do not have enough money for that item!");
+	}
+});
+$("#banana").on('click', 'button', function () {
+	if (totalValue > banana.price) {
+		totalValue -= banana.price;
+		console.log('total value', totalValue);
+		$('#totalValue').text(totalValue.toFixed(2));
+		buyerBanana.buyerBananaAmount++;
+		$('#bananaAmount').text(buyerBanana.buyerBananaAmount);
+		buyerBanana.buyerBananaTotalPrice+=banana.price;
+		$('#totalBananaPrice').text((buyerBanana.buyerBananaTotalPrice).toFixed(2));
+		buyerBanana.buyerBananaAveragePrice=buyerBanana.buyerBananaTotalPrice/buyerBanana.buyerBananaAmount;
+		$('#averageBananaPrice').text((buyerBanana.buyerBananaAveragePrice).toFixed(2));
+	} else {
+		alert("You do not have enough money for that item!");
+	}
+});
+$("#grape").on('click', 'button', function () {
+	if (totalValue > grape.price) {
+		totalValue -= grape.price;
+		console.log('total value', totalValue);
+		$('#totalValue').text(totalValue.toFixed(2));
+		buyerGrape.buyerGrapeAmount++;
+		$('#grapeAmount').text(buyerGrape.buyerGrapeAmount);
+		buyerGrape.buyerGrapeTotalPrice+=grape.price;
+		$('#totalGrapePrice').text((buyerGrape.buyerGrapeTotalPrice).toFixed(2));
+		buyerGrape.buyerGrapeAveragePrice=buyerGrape.buyerGrapeTotalPrice/buyerGrape.buyerGrapeAmount;
+		$('#averageGrapePrice').text((buyerGrape.buyerGrapeAveragePrice).toFixed(2));
+	} else {
+		alert("You do not have enough money for that item!");
+	}
+});
 
 var interval;
 function myFunction (){
